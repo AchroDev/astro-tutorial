6d1f5eb301be62ed36ba5a02fe6390c253a1b3f3
AchroDev <achrodev@proton.me>
2025-11-12 23:56:07 -0600

    Added Preact UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://earnest-beignet-b23b87.netlify.app/"
+  site: "https://earnest-beignet-b23b87.netlify.app/",
+  integrations: [preact()]
 });
